11f54463b5669e738050d0f095af92ad10fb90f2
HimanshuChittora23908 <2020kucp1023@iiitkota.ac.in>
2021-07-17 16:37:43 +0530

    fa icons

--- src/footer.js
+++ src/footer.js
@@ -1,104 +1,105 @@
 import React from 'react';
 import './footer.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 export default function Footer() {
     return (
         <div>
-<footer class="text-center text-lg-start bg-light text-muted">
+<footer className="text-center text-lg-start bg-light text-muted">
   <section
-    class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+    className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
   >
-    <div class="me-5 d-none d-lg-block">
+    <div className="me-5 d-none d-lg-block">
       <span>Get connected with us on social networks:</span>
     </div>
     <div>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-facebook-f"></i>
+      <a href="" className="me-4 text-reset">
+        <i className="fa fa-facebook"></i>
       </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-twitter"></i>
+      <a href="" className="me-4 text-reset">
+        <i className="fa fa-twitter"></i>
       </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-google"></i>
+      <a href="" className="me-4 text-reset">
+        <i className="fa fa-google"></i>
       </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-instagram"></i>
+      <a href="" className="me-4 text-reset">
+        <i className="fa fa-instagram"></i>
       </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-linkedin"></i>
+      <a href="" className="me-4 text-reset">
+        <i className="fa fa-linkedin"></i>
       </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-github"></i>
+      <a href="" className="me-4 text-reset">
+        <i className="fa fa-github"></i>
       </a>
     </div>
   </section>
-  <section class="">
-    <div class="container text-center text-md-start mt-5">
-      <div class="row mt-3">
-        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-          <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3 gem"></i>Team AZTECS
+  <section className="">
+    <div className="container text-center text-md-start mt-5">
+      <div className="row mt-3">
+        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <h6 className="text-uppercase fw-bold mb-4">
+            <i className="fa fa-hashtag me-3"></i>Team AZTECS
           </h6>
           <p>
             Here you can use rows and columns to organize your footer content. Lorem ipsum
             dolor sit amet, consectetur adipisicing elit.
           </p>
         </div>
-        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-          <h6 class="text-uppercase fw-bold mb-4">
+        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <h6 className="text-uppercase fw-bold mb-4">
             Products
           </h6>
           <p>
-            <a href="#!" class="text-reset">Notes</a>
+            <a href="#!" className="text-reset">Notes</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Study Material</a>
+            <a href="#!" className="text-reset">Study Material</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Contests</a>
+            <a href="#!" className="text-reset">Contests</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Recordings</a>
+            <a href="#!" className="text-reset">Recordings</a>
           </p>
         </div>
-        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-          <h6 class="text-uppercase fw-bold mb-4">
+        <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <h6 className="text-uppercase fw-bold mb-4">
             Useful links
           </h6>
           <p>
-            <a href="#!" class="text-reset">About Us</a>
+            <a href="#!" className="text-reset">About Us</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Team</a>
+            <a href="#!" className="text-reset">Team</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Contact Us</a>
+            <a href="#!" className="text-reset">Contact Us</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">FAQ</a>
+            <a href="#!" className="text-reset">FAQ</a>
           </p>
         </div>
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-          <h6 class="text-uppercase fw-bold mb-4">
+        <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 className="text-uppercase fw-bold mb-4">
             Contact
           </h6>
-          <p><i class="fas fa-home me-3"></i> Jaipur, Pin: 302017, Rajasthan, India</p>
+          <p><i className="fa fa-home me-3"></i> Jaipur, Pin: 302017, Rajasthan, India</p>
           <p>
-            <i class="fas fa-envelope me-3"></i>
+            <i className="fa fa-envelope me-3"></i>
             himanshuhome908@gmail.com
           </p>
-          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+          <p><i className="fa fa-phone me-3"></i> + 01 234 567 88</p>
+          <p><i className="fa fa-print me-3"></i> + 01 234 567 89</p>
         </div>
       </div>
     </div>
   </section>
 
-  <div class="text-center p-4">
+  <div className="text-center p-4">
     © 2021 Copyright :
-    <a class="text-reset fw-bold"> Team Aztecs </a>
+    <a className="text-reset fw-bold"> Team Aztecs </a>
     <p>Maintainer - Himanshu Chittora</p>
-    <p class="maintainer"> Email @ <a href="mailto:2020kucp1023@iiitkota.ac.in" className="mail_maintainer">2020kucp1023@iiitkota.ac.in</a></p>
+    <p className="maintainer"> Email @ <a href="mailto:2020kucp1023@iiitkota.ac.in" className="mail_maintainer">2020kucp1023@iiitkota.ac.in</a></p>
   </div>
 </footer>
         </div>
