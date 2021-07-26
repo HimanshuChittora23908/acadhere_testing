c5c3411fad1c63fe1e829b72dce7adbc4c801ba9
HimanshuChittora23908 <2020kucp1023@iiitkota.ac.in>
2021-07-26 07:11:37 +0530

    contest-changes

--- src/Contest.js
+++ src/Contest.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import './Contest.css'
+import Clock from "../src/images/Clock.png";
+import Hourglass from "../src/images/Hourglass.png";
 
 const Contest = () => {
   const [contest, setContest] = useState(null)
@@ -9,20 +12,27 @@ const Contest = () => {
   }, [])
 
   const getList = async () => {
-    axios.get("https://backend-clg-app.herokuapp.com/cp_reminder/")
+    axios.get("https://backend-clg-app.herokuapp.com/cp_reminder?pg=1")
     .then((data) => {
           setContest(data.data)
         })
 }
 
+
+
   return (
-    <div>
-      {contest ? contest.map((item,ind) => 
+    <div className="bg_contest">
+      {contest ? contest.contest.map((item,ind) => 
       <div key={ind}>
+        <div className="card_contest">
+        <div className="card_contest-body"></div>
         <h2>{item.event_name}</h2>
-        <h4>{new Date(`${item.start_time}`).toTimeString()}</h4>
-        <h4>{new Date(0).setSeconds(`${item.duration}`).toISOString().substr(11,8)}</h4>
+        <h5><img src={Clock} className="Clock"></img> {(`${item.start_time}`).slice(0,15)}, {new Date(`${item.start_time}`).toTimeString().slice(0,8)}</h5>
+        <h5><img src={Hourglass} className="Hourglass"></img> {new Date(`${item.duration}`*1000).toISOString().substr(11,8)}</h5>
+        <h6 className="contest_website">{item.resource_website}</h6>
+        <span className="flip_button"><a href={item.contest_url}></a></span>
         </div>
+      </div>
       ) : null}
     </div>
   )
